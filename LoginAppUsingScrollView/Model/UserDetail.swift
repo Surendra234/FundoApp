d277c619dc0679a27b53c00862a5c65e6f2cef7f
surendra <surendramahawar873@gmail.com>
2022-08-10 21:11:50 +0530

    [surendra] .feat profile image option

---
+++ LoginAppUsingScrollView/Model/UserDetail.swift
@@ -0,0 +1,15 @@
+//
+//  UserDetail.swift
+//  LoginAppUsingScrollView
+//
+//  Created by Admin on 10/08/22.
+//
+
+import Foundation
+
+struct UserProfile {
+    
+    var username: String?
+    var email: String?
+    var photoUrl: String?
+}
---
+++ LoginAppUsingScrollView/NetworkCall/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  LoginAppUsingScrollView
+//
+//  Created by Admin on 10/08/22.
+//
+
+import Foundation
